0e59abba1589bd06502252ac966e364d38dbec3b
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-08-23 03:16:54 +0900

    RL Agent 학습 설정 완료

---
+++ createDefaultAgent.m
@@ -0,0 +1,110 @@
+
+
+
+
+% set Env
+observationDim = [3 1];
+observationInfo = rlNumericSpec(observationDim);
+
+actionDim = [2 1];
+actionInfo = rlNumericSpec( ...
+    actionDim, ...
+    'LowerLimit', 0, ...
+    'UpperLimit', 1 ...
+);
+
+HESS_Env = rlSimulinkEnv( ...
+    'HESS', ...
+    'HESS/Load Split Controller/RL/Agent', ...
+    observationInfo, actionInfo ...
+);
+
+
+
+
+% actor
+actnet = [
+    featureInputLayer(3, 'Name', 'obs')
+    fullyConnectedLayer(64, 'Name', 'hidden1')
+    reluLayer('Name', 'relu1')
+    fullyConnectedLayer(64, 'Name', 'hidden2')
+    reluLayer('Name', 'relu2')
+    fullyConnectedLayer(64, 'Name', 'hidden3')
+    reluLayer('Name', 'relu3')
+    fullyConnectedLayer(2, 'Name', 'act_before')
+    softmaxLayer('Name', 'act')
+];
+
+actor = rlDeterministicActorRepresentation( ...
+    actnet, ...
+    observationInfo, ...
+    actionInfo, ...
+    "Observation",'obs','Action', 'act' ...
+);
+
+% critic
+obsPath = [
+    featureInputLayer(3, 'Name','obs')
+    fullyConnectedLayer(64, 'Name', 'hidden1')
+    reluLayer('Name', 'relu1')
+    fullyConnectedLayer(64, 'Name', 'hidden2')
+    reluLayer('Name', 'relu2')
+    additionLayer(2, 'Name', 'add')
+    reluLayer('Name', 'relu3')
+    fullyConnectedLayer(64, 'Name', 'hidden3')
+    reluLayer('Name', 'relu4')
+    fullyConnectedLayer(1, 'Name', 'value')
+];
+
+actPath = [
+    featureInputLayer(2, 'Normalization','none', 'Name','act')
+    fullyConnectedLayer(64, 'Name', 'fcact')
+];
+
+qvalnet = layerGraph(obsPath);
+qvalnet = addLayers(qvalnet, actPath);
+qvalnet = connectLayers(qvalnet, 'fcact', 'add/in2');
+
+critic = rlQValueRepresentation( ...
+    qvalnet, observationInfo, actionInfo, ...
+    'Observation', 'obs', 'Action', 'act' ...
+);
+
+% agent
+
+options = rlDDPGAgentOptions;
+options.ExperienceBufferLength = 1000000;
+options.DiscountFactor = 0.999;
+options.TargetSmoothFactor = 0.005;
+
+
+agent = rlDDPGAgent(actor, critic, options);
+
+
+
+% clear trash
+clear actionDim;
+clear actionInfo;
+clear actnet;
+clear actor;
+clear actPath;
+clear critic;
+clear observationDim;
+clear observationInfo;
+clear obsPath;
+clear options;
+clear qvalnet;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- setEnv.m
+++ setEnv.m
@@ -2,43 +2,17 @@
 clear
 clearvars
 
-
-% Power Demand
-load Drive-Cycle/FTP72_Power.mat
-load Drive-Cycle/FTP75_Power.mat
-load Drive-Cycle/UDDS_Power.mat
-load Drive-Cycle/WLTP_Class1_Power.mat
-load Drive-Cycle/WLTP_Class2_Power.mat
-load Drive-Cycle/WLTP_Class3_Power.mat
-
-%{
-Power_Demand.FTP72 = FTP72;
-Power_Demand.FTP75 = FTP75;
-Power_Demand.UDDS = UDDS;
-Power_Demand.WLTP_Class1 = WLTP_Class_1;
-Power_Demand.WLTP_Class2 = WLTP_Class_2;
-Power_Demand.WLTP_Class3 = WLTP_Class_3;
-%}
+disp('=======Env Configuration Start!=======')
 
 
-% Battery Parameter
-Battery.NominalVoltage = 340; % V
-Battery.InternalResistance = 0.01; % Ohm
-Battery.Capacity = 176.47; % Ahr
-Battery.V1 = 306;
-Battery.AH1 = 88.235;
-Battery.InitialCharge = 123.53; % Ahr
+disp('clear vars ...')
 
 
-% Capacitor Parameter
-Capacitor.Cell_Voltage = 2.7; % V
-Capacitor.Cell_Capacitance = 2200; % F
-Capacitor.Cell_InternalResistance = 0.47; % mOhm
-Capacitor.N_Serial = 222;
+setHESSParam
+disp('Load HESS Param ...')
 
-Capacitor.Voltage = Capacitor.Cell_Voltage * Capacitor.N_Serial; % V
-Capacitor.Capacitance = 1 / (Capacitor.N_Serial / Capacitor.Cell_Capacitance); % F
-Capacitor.InternalResistance = Capacitor.Cell_InternalResistance * Capacitor.N_Serial * 0.001; % Ohm
-Capacitor.Coulomb = Capacitor.Voltage * Capacitor.Capacitance; % C
-Capacitor.Capacity = Capacitor.Coulomb / 3600; % Ahr
+createDefaultAgent
+disp('Create Default DDPG Agent ...')
 
+
+disp('=====Env Configuration Finished!=====')
