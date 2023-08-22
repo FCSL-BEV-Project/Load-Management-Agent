3ebd8da615ae515ab7052bfe173bf880ec7bebc4
Taeuk Ahn <tu1014@naver.com>
2023-08-22 14:51:04 +0900

    Power Demand Ref 파일 추가

--- setEnv.m
+++ setEnv.m
@@ -3,7 +3,22 @@ clear
 clearvars
 
 
+% Power Demand
+load Drive-Cycle/FTP72_Power.mat
 load Drive-Cycle/FTP75_Power.mat
+load Drive-Cycle/UDDS_Power.mat
+load Drive-Cycle/WLTP_Class1_Power.mat
+load Drive-Cycle/WLTP_Class2_Power.mat
+load Drive-Cycle/WLTP_Class3_Power.mat
+
+%{
+Power_Demand.FTP72 = FTP72;
+Power_Demand.FTP75 = FTP75;
+Power_Demand.UDDS = UDDS;
+Power_Demand.WLTP_Class1 = WLTP_Class_1;
+Power_Demand.WLTP_Class2 = WLTP_Class_2;
+Power_Demand.WLTP_Class3 = WLTP_Class_3;
+%}
 
 
 % Battery Parameter
@@ -19,7 +34,7 @@ Battery.InitialCharge = 123.53; % Ahr
 Capacitor.Cell_Voltage = 2.7; % V
 Capacitor.Cell_Capacitance = 1500; % F
 Capacitor.Cell_InternalResistance = 0.47; % mOhm
-Capacitor.N_Serial = 300;
+Capacitor.N_Serial = 222;
 
 Capacitor.Voltage = Capacitor.Cell_Voltage * Capacitor.N_Serial; % V
 Capacitor.Capacitance = 1 / (Capacitor.N_Serial / Capacitor.Cell_Capacitance); % F
