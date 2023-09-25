d70d46774d386d846d3ec2f1b3c73722dee5d365
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-09-25 18:12:22 +0900

    create_SOC_data

--- create_SOC_dataset.m
+++ create_SOC_dataset.m
@@ -3,36 +3,42 @@ clearvars
 clear
 
 setEnv;
-
 modelName = "HESS";
 
 idx = 1;
-rates = 1:-0.005:0.75;
+rates = linspace(1, 0.7, 50);
 len = size(rates, 2);
-
+time = ["1372" "2474" "1369" "1022" "1477" "1800"];
 
 disp('========start simulation!========')
 
-for r = rates
 
-    Split_Rate = r;
-    res = sim(modelName, "StopTime", "1800");
+for i = 1:6
+
+    sim_case = i;
+
+    for r = rates
+
+        Split_Rate = r;
+        res = sim(modelName, "StopTime", time(i));
+        
     
-
-    tmp = [
-        res.HSOC.signals(1).values';
-        res.HSOC.signals(2).values';
-        res.HSOC.signals(3).values';
-        res.HSOC.signals(4).values';
-        res.HSOC.signals(5).values';
-    ];
-
-    data{idx} = tmp;
-
-    percentage = (idx*100)/len;
-    disp(percentage)
-
-    idx = idx + 1;
+        tmp = [
+            res.HSOC.signals(1).values';
+            res.HSOC.signals(2).values';
+            res.HSOC.signals(3).values';
+            res.HSOC.signals(4).values';
+            res.HSOC.signals(5).values';
+        ];
+    
+        data{idx} = tmp;
+    
+        percentage = (idx*100)/(len*6);
+        disp(percentage)
+    
+        idx = idx + 1;
+    
+    end
 
 end
 
