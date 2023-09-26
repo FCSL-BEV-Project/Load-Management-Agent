62303a7a27705136c9cc7968a4a8db8e4015cd37
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-09-26 14:09:55 +0900

    dqn_test

--- setEnv.m
+++ setEnv.m
@@ -11,9 +11,11 @@ disp('clear vars ...')
 setHESSParam
 disp('Load HESS Param ...')
 
-createDDPGAgent
+% createDDPGAgent
 % createSACAgent
-disp('Create Default RL Agent ...')
+disp('set RL env ...')
+setRLEnv;
+% load RL_Agent/dqn_agent.mat;
 
 
 disp('=====Env Configuration Finished!=====')
---
+++ setRLEnv.m
@@ -0,0 +1,29 @@
+
+% set Env
+observationDim = [5 1];
+observationInfo = rlNumericSpec(observationDim);
+
+%{
+actionDim = [1 1];
+actionInfo = rlNumericSpec( ...
+    actionDim, ...
+    'LowerLimit', 0, ...
+    'UpperLimit', 1 ...
+);
+%}
+
+choices = 1:4;
+actionInfo = rlFiniteSetSpec(choices);
+
+
+HESS_Env = rlSimulinkEnv( ...
+    'HESS', ...
+    'HESS/RL-based EMS/RL/Agent', ...
+    observationInfo, actionInfo ...
+);
+
+clear observationDim;
+clear observationInfo;
+
+clear choices;
+clear actionInfo;
