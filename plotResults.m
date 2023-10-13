66d0dffd3c81d97e15034489e125dbdc591292d8
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-10-13 15:59:41 +0900

    update results

--- plotResults.m
+++ plotResults.m
@@ -1,16 +1,19 @@
 
-load Results\Active_Bat_SOC.mat
-load Results\Active_Cap_Cycle.mat
-load Results\Passive_Bat_SOC.mat
-load Results\Passive_Cap_Cycle.mat
-load Results\RL_Bat_SOC.mat
-load Results\RL_Cap_Cycle.mat
+load Results/Active_Bat_SOC.mat
+load Results/Active_Cap_Cycle.mat
+load Results/Passive_Bat_SOC.mat
+load Results/Passive_Cap_Cycle.mat
+load Results/RL_Bat_SOC.mat
+load Results/RL_Cap_Cycle.mat
+load Results/RL_Bat_SOH.mat
+load Results/Active_Bat_SOH.mat
+load Results/Passive_Bat_SOH.mat
 
 time = Active_Bat_SOC.time;
 
 figure(1)
 
-subplot(2, 1, 2)
+subplot(3, 1, 2)
 hold on
 
 plot(time, Active_Bat_SOC.signals.values, 'r', 'DisplayName', 'Active Assist')
@@ -23,7 +26,7 @@ xlabel('time')
 
 legend()
 
-subplot(2, 1, 1)
+subplot(3, 1, 1)
 hold on
 
 plot(time, Active_Cap_Cycle.signals.values(:,:), 'r', 'DisplayName', 'Active Assist')
@@ -35,6 +38,18 @@ xlabel('time')
 
 legend()
 
+subplot(3, 1, 3)
+hold on
 
+time = 1:size(RL_Bat_SOH, 2);
+
+plot(time, RL_Bat_SOH, 'b', 'DisplayName', 'Combined Assist')
+plot(time, Passive_Bat_SOH, 'g', 'DisplayName', 'Passive Assist')
+plot(time, Active_Bat_SOH, 'r', 'DisplayName', 'Active Assist')
+
+title('Battery SOH')
+xlabel('N Sim')
+
+legend()
 
 
--- sim_SOH.m
+++ sim_SOH.m
@@ -7,11 +7,13 @@ setEnv;
 
 i = 1:100;
 Bat_SOH = zeros(1, size(i, 2), 'double');
-Bat_SOH = [];
 
 for index = i
+
     res = sim('HESS.slx');
+
     Bat_SOH(index) = res.Bat_SOH.signals.values(:,:,end);
+
     Battery.SOH = res.Bat_SOH.signals.values(:,:,end);
     Battery.Capacity = Battery.InitialCapacity * Battery.SOH; % Ahr
     Battery.InitialCharge = Battery.InitialSOC * Battery.Capacity; % Ahr
