860c36c2f6e7403177ab5cb454b4d0e40571a78b
Taeuk Ahn <tu1014@naver.com>
2023-11-07 16:55:42 +0900

    train DDPG Agent

--- setEnv.m
+++ setEnv.m
@@ -7,6 +7,7 @@ disp('Load HESS Param ...')
 disp('set RL env ...')
 % setDQNEnv;
 % load RL_Agent/DQN-0929-1.mat;
+load RL_Agent/DDPG_Agent_Trained_1106.mat
 setDDPGEnv
 
 disp('=====Env Configuration Finished!=====')
