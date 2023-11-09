f7a8d27a14820f0ea034491711338330113ee792
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-11-09 18:16:13 +0900

    add DQN System

--- setEnv.m
+++ setEnv.m
@@ -5,9 +5,10 @@ setHESSParam
 disp('Load HESS Param ...')
 
 disp('set RL env ...')
-% setDQNEnv;
+
 % load RL_Agent/DQN-0929-1.mat;
-load RL_Agent/DDPG_Agent_Trained_1106.mat
-setDDPGEnv
+% load RL_Agent/DDPG_Agent_Trained_1106.mat
+% setDDPGEnv
+setDQNEnv;
 
 disp('=====Env Configuration Finished!=====')
