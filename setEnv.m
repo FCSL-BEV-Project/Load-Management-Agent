3a1d275d2514ddc083729f38640fae2ff0354709
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2023-12-15 08:20:19 +0900

    implement Ref method

--- setEnv.m
+++ setEnv.m
@@ -8,8 +8,9 @@ disp('set RL env ...')
 
 % load RL_Agent/DQN-0929-1.mat;
 % load RL_Agent/DDPG_Agent_Trained_1106.mat
-setDDPGEnv
-load RL_Agent/Default_DDPG_Agent.mat
+% setDDPGEnv
+setRefEnv
+% load RL_Agent/Default_DDPG_Agent.mat
 % setDQNEnv;
 
 disp('=====Env Configuration Finished!=====')
