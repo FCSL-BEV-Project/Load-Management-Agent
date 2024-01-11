7e19e7c455c2eee857a659cb3eff7c0739733f67
Taeuk Ahn <75111858+tu1014@users.noreply.github.com>
2024-01-12 01:35:46 +0900

    update reward funciton

--- plot_multival.m
+++ plot_multival.m
@@ -3,8 +3,9 @@ y = linspace(0, 1, 11); % UC Rate
 
 [X, Y] = meshgrid(x, y);
 
-F = X .* Y;
-%F = (X .* 0.2) .^ (Y .* 0.3);
+% F = X .* Y;
+
+F = ((X .* 0.5) .^ (Y .* 0.5));
 
 % surf(X, Y, F);
 bar3(F);
